14dc42b19243462be8370ac0ffcce07cec93e496
Sander <developer@golles.nl>
2023-06-03 18:05:41 +0000

    Add code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/node_modules/**', '!**/vendor/**'],
+  coverageReporters: ['text', 'cobertura'],
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
   transform: {
